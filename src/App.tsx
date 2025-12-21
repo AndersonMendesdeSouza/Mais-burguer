60d546fae9bfde5848acb3e4154bc42918863412
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2025-12-21 17:44:02 -0300

    fix adjustements

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Cart from "./pages/cart/Cart";
-import FoodDetails from "./pages/food/FoodDetails";
 import Main from "./pages/main/Main";
 import Checkout from "./pages/checkout/checkout";
+import FoodDetails from "./pages/food/FoodDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/sobre" element={<FoodDetails />} />
       <Route path="/foodDetails" element={<FoodDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<Checkout />} />
